3900dc467c1dce81318b73ff180a4f0ffbe09bd3
Kakumirizi Daud <dkakumirizii@gmail.com>
2019-02-03 21:03:07 -0800

    added a static block and javadoc comment

--- src/ObjectOriented/Programming.java
+++ src/ObjectOriented/Programming.java
@@ -1,13 +1,31 @@
 package ObjectOriented;
 
+@interface Kakumirizi{
+	String name () default " ";
+	String email () default " ";
+}
+@Kakumirizi (name = "Kakumirizi Daud",
+	email = "dkakumirzii@gmail.com")
 public class Programming {
+	static String continent;
+	static 
+	{
+		continent = "Africa, the mother land";
+		System.out.println(continent);
+	}
+	/**
+	 * this class is excuted by creating an object in the main method
+	 *
+	 * */
 	public  static void flip()
 	{
 		StringBuffer sb = new StringBuffer("Software ");
 		sb.append("Engineering");
 		System.out.println(sb);
 	}
+	
 	public static void main(String[] args) {
+		
 		Programming.flip();
 		
 	}
