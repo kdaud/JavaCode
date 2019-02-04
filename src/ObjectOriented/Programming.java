6b897e83ccfd692840a5834ff52fef4633ce2e01
Kakumirizi Daud <dkakumirizii@gmail.com>
2019-02-03 20:45:11 -0800

    Initial commit

---
+++ src/ObjectOriented/Programming.java
@@ -0,0 +1,14 @@
+package ObjectOriented;
+
+public class Programming {
+	public  static void flip()
+	{
+		StringBuffer sb = new StringBuffer("Software ");
+		sb.append("Engineering");
+		System.out.println(sb);
+	}
+	public static void main(String[] args) {
+		Programming.flip();
+		
+	}
+}
